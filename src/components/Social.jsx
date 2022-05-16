192868e5ba958e7d3537b32002140ed53db3d5a3
MrRamoun <omartarekramoun@gmail.com>
2022-05-16 04:31:09 +0200

    Update Social section of contact Page

--- src/components/Social.jsx
+++ src/components/Social.jsx
@@ -2,15 +2,15 @@ import React from "react";
 
 const SocialShare = [
   {
-    iconName: "fa fa-facebook",
-    link: "https://www.facebook.com/",
+    iconName: "fa fa-github",
+    link: "https://www.github.com/MrRamoun",
   },
-  { iconName: "fa fa-twitter", link: "https://twitter.com/" },
+  { iconName: "fa fa-twitter", link: "https://twitter.com/ramoun16" },
   {
-    iconName: "fa fa-youtube",
-    link: "https://www.youtube.com/",
+    iconName: "fa fa-linkedin",
+    link: "https://www.linkedin.com/in/ramoun",
   },
-  { iconName: "fa fa-dribbble", link: "https://dribbble.com/" },
+  { iconName: "fa fa-angellist", link: "https://angel.co/omar-ramoun" },
 ];
 
 const Social = () => {
