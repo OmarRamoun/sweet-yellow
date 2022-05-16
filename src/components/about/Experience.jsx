831a2f644325f684a8024f942e08e140067a0df2
MrRamoun <omartarekramoun@gmail.com>
2022-05-16 04:20:39 +0200

    Update Experience section in about page

--- src/components/about/Experience.jsx
+++ src/components/about/Experience.jsx
@@ -1,26 +1,45 @@
 import React from "react";
+import ReactMarkdown from 'react-markdown';
+import remarkGfm from 'remark-gfm';
 
 const experienceContent = [
   {
-    year: "   2018 - Present",
-    position: " Web Developer",
-    compnayName: "Envato",
-    details: `  Lorem ipsum dolor sit amet, consectetur tempor incididunt ut labore
-        adipisicing elit`,
+    year: "2021 - 2022",
+    position: "Full-stack Developer",
+    compnayName: "Xoommit",
+    details: ``,
   },
   {
-    year: "2013 - 2018",
-    position: " UI/UX Designer",
-    compnayName: "Themeforest",
-    details: `Lorem incididunt dolor sit amet, consectetur eiusmod dunt doldunt dol
-        elit, tempor incididunt`,
+    year: "2021 - 2022",
+    position: "Mentor Junior Developers",
+    compnayName: "Microverse",
+    details:
+    `✔️ Helped new students adapt to remote learning at Microverse.
+
+✔️ Helped students overcome their self-doubts (including imposter syndrome). Helped students with personal issues and taught them how to overcome those kinds of problems in the future.
+
+✔️ Gave Students technical help when needed.
+
+✔️ Helped students build self-trust in themselves during learning blocks and tutorial rabbit holes.
+
+✔️ Helped students by giving them advice about how to work and learn remotely and how to overcome the problems associated with remote working (e.g: distractions, bad working environment, communicating with other remote workers professionally).`,
   },
   {
-    year: "2005 - 2013",
-    position: "Consultant",
-    compnayName: "Videohive",
-    details: `Lorem ipsum dolor sit amet, tempor incididunt ut laboreconsectetur
-        elit, sed do eiusmod tempor duntt`,
+    year: "2019 - 2021",
+    position: "Software Engineer",
+    compnayName: "AlexLab",
+    details:
+    `✔️ Adapted to new frameworks fast to deliver Full-fledged Apps.
+
+✔️ Participated in the creation of a CSS-in-js library for serving    demanding projects requirements.
+
+✔️ Worked with UX designers to design and build user interfaces and iterate based on user feedback.
+
+✔️ Dealt with design software (Figma, Adobe xd, Photoshop, Illustrator).
+
+✔️ Implemented different reverse-engineering techniques to work with vendor-software UI.
+
+✔️ Used multiple C++ frameworks and libraries to implement UI Solutions.`,
   },
 ];
 
@@ -37,7 +56,7 @@ const Experience = () => {
             {val.position}
             <span className="place open-sans-font">{val.compnayName}</span>
           </h5>
-          <p className="open-sans-font">{val.details}</p>
+          <ReactMarkdown className="open-sans-font" children={val.details} remarkPlugins={[remarkGfm]} />
         </li>
       ))}
     </ul>
