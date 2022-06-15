1bd06c842749603cf6435647243b4755ffbeca21
MrRamoun <omartarekramoun@gmail.com>
2022-06-15 05:40:16 +0200

    Fix a typo in class name

--- src/components/Address.jsx
+++ src/components/Address.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const Address = () => {
   return (
     <>
-      <p className="open-sans-font cutom-span-contact position-relative">
+      <p className="open-sans-font custom-span-contact position-relative">
         <i className="fa fa-envelope-open position-absolute"></i>
         <span className="d-block">mail me</span>{" "}
         <a href="mailto:steve@mail.com">contact@omarramoun.com</a>
